66c511f0f7de52379cb0a1ba99d26c13361c24e5
shanli <shanli@2dfire.com>
2017-08-23 16:15:13 +0800

    new

---
+++ Example/Pods/Target Support Files/ChaihuLeftDrawer/ChaihuLeftDrawer-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double ChaihuLeftDrawerVersionNumber;
+FOUNDATION_EXPORT const unsigned char ChaihuLeftDrawerVersionString[];
+
